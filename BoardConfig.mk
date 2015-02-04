07cb5e919a2ae8d2e9146db1c6fa75797017a0c9
Trevor Drake <trevordrake.gerrit@gmail.com>
2015-02-04 11:23:37 +0000

    Fix BOARD_MKBOOTIMG_ARGS , BOARD_RAMDISK_OFFSET, BOARD_KERNEL_TAGS_OFFSET
    The ramdisk_offset and tags_offset where incorrect
    BOARD_MKBOOTIMG_ARGS seemed to be creating the boot image correctly but
    only by be luck as mkbootimg was parsing the "B" from the variable name as the
    value, this is the correct value.
    
    The factory image values are
    
    ramdisk_addr     :0x0000000b
    tags_addr        :0x0000000b
    
    Change-Id: Ia815185aa9f23df684f0798a9c09ef6998fa630b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,12 +29,12 @@ BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 0x0000000b
+BOARD_RAMDISK_OFFSET     := 0x0000000b
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=permissive androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M
 
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
