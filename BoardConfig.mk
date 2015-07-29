b3706e89b9d2c5fe9ed3bb6ee3b110c4e9aa7a1d
APOPHIS9283 <steaven.android@gmail.com>
2015-07-29 19:38:36 -0400

    Revert "shamu: healthd: Add tricolor led support"
    This reverts commit 683ce4ce953c035133a9d35bd9577b7c8c57f8f2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,9 +189,6 @@ BOARD_HARDWARE_CLASS := device/moto/shamu/cmhw
 # Block_Build
 Bliss_Build_Block:=1 
 
-# Healthd
-HEALTHD_ENABLE_TRICOLOR_LED := true
-
 # TWRP
 TW_THEME := portrait_hdpi
 TW_INCLUDE_L_CRYPTO := true
