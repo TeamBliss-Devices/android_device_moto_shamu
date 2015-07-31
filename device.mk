d4e5283ed5ba1a5b676eceffedeeab5cee133d6c
APOPHIS9283 <steaven.android@gmail.com>
2015-07-30 21:17:22 -0400

    Revert "Lower DPI to 500"
    This reverts commit d04f303227c590390f00f51077f50891fbfcf131.

--- device.mk
+++ device.mk
@@ -246,7 +246,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196609
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=500
+    ro.sf.lcd_density=560
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
