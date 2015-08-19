24e67d3dca4a5f96d8a171ad12c2e6b827d8c871
APOPHIS9283 <steaven.android@gmail.com>
2015-08-19 17:58:15 -0400

    Disable GPS Debug
    Change-Id: I63b0b557064cd8dfc149e6d2974e14e6eaea97bd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ TARGET_TC_KERNEL := 6.0-linaro
 TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
 TARGET_ENABLE_UKM := true
-
 WITH_LZMA_OTA := true
+
 #SaberMod
 -include vendor/bliss/config/sm.mk
