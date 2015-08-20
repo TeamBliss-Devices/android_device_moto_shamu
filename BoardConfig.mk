5ae543588dd27346ec9203442d066eb1bbd4b740
Shaun <shaunsibu@gmail.com>
2015-08-19 23:10:34 -0600

    Add userdebug to dexpreopt
    Change-Id: Ife09f219aceecb5d7ec41881b82f6c590f64267c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ TARGET_CRYPTFS_HW_PATH := device/moto/shamu/cryptfs_hw
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
+  ifeq ($(TARGET_BUILD_VARIANT),user userdebug)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
