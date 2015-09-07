d997a9d91d7d77aa75e4cd4a4f9baa04be632405
Michael Bestas <mikeioannina@gmail.com>
2015-09-07 19:48:47 -0400

    SHAMU: optimize ART
    --Turn off ART watchdog
    
    There is a time limit for optimization of apk.
    Some apps like play services takes a lot of time,
    so it doesn't gets optimized properly due to time limit
    and tries to optimize on each both.It also happens when it's updated.
    By disabling watchdog we let the apk to get optimized completely.
    
    Change-Id: I3808417b0f9f6a9602dd3aff8dfa6b972ae5f47d

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=1
 
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-flags=--no-watch-dog
+
 # Audio effects
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
