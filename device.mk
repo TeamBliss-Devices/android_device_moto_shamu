74281fedf982f3a85a2113a2ccad47ff24a93dce
mydongistiny <jaysonedson@gmail.com>
2015-09-23 10:58:13 -0600

    Change default density to 500
    Change-Id: Ia1a1f38bba82eadb013dffe0201bff415021ff76

--- device.mk
+++ device.mk
@@ -237,7 +237,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196609
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=560
+    ro.sf.lcd_density=560 \
+    persist.sys.lcd_density=500
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
